7b8e1a126b12bb2c856cc70214863b862dba4a54
Shreyash <saitwalshreyash19@gmail.com>
2021-04-04 21:30:19 +0530

    Add support for generating rush.yml

--- migrator/src/main/java/io/shreyash/rush/migrator/Migrator.java
+++ migrator/src/main/java/io/shreyash/rush/migrator/Migrator.java
@@ -5,8 +5,10 @@ import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.annotations.UsesActivities;
 import com.google.appinventor.components.annotations.UsesActivityMetadata;
 import com.google.appinventor.components.annotations.UsesApplicationMetadata;
+import com.google.appinventor.components.annotations.UsesAssets;
 import com.google.appinventor.components.annotations.UsesBroadcastReceivers;
 import com.google.appinventor.components.annotations.UsesContentProviders;
+import com.google.appinventor.components.annotations.UsesLibraries;
 import com.google.appinventor.components.annotations.UsesPermissions;
 import com.google.appinventor.components.annotations.UsesServices;
 import com.google.appinventor.components.annotations.androidmanifest.ActivityElement;
@@ -19,6 +21,8 @@ import com.google.auto.service.AutoService;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Set;
 
@@ -35,6 +39,7 @@ import javax.tools.Diagnostic;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -74,13 +79,22 @@ public class Migrator extends AbstractProcessor {
   @Override
   public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnv) {
     final String manifestPath = processingEnv.getOptions().get("manifestPath");
+    final String extName = processingEnv.getOptions().get("extName");
+    final String rushYmlPath = processingEnv.getOptions().get("rushYmlPath");
+
     final Messager messager = processingEnv.getMessager();
 
     for (Element el : roundEnv.getElementsAnnotatedWith(DesignerComponent.class)) {
       SimpleObject so = el.getAnnotation(SimpleObject.class);
       if (so.external()) {
-        generateAndroidManifest(el, messager, manifestPath);
-        messager.printMessage(Diagnostic.Kind.NOTE, "External component class named \"" + el.getSimpleName().toString() + "\" detected.");
+        messager.printMessage(Diagnostic.Kind.NOTE, "External component class named \"" +
+            el.getSimpleName().toString() + "\" detected.");
+        try {
+          generateAndroidManifest(el, manifestPath);
+          generateRushYml(el, extName, rushYmlPath);
+        } catch (TransformerException | ParserConfigurationException | IOException e) {
+          e.printStackTrace();
+        }
       }
     }
 
@@ -88,24 +102,80 @@ public class Migrator extends AbstractProcessor {
   }
 
   /**
+   * Generates rush.yml for {@param comp}.
+   *
+   * @param comp        the element for which rush.yml is to be produced
+   * @param extName     the name of the extension which is to be produced
+   * @param rushYmlPath the path where the generated rush.yml is to be stored
+   */
+  private void generateRushYml(Element comp, String extName, String rushYmlPath) throws IOException {
+    final String moreInfo = "# For a detailed info on this file and supported fields, check out this" +
+        "\n# link: https://github.com/ShreyashSaitwal/rush-cli/wiki/Metadata-File\n";
+    final String optimizeComment = "# Un-comment the below field if you wish to apply ProGuard while" +
+        " building\n# a release build ('-r') of your extension:\n# release:\n#   optimize: true\n";
+
+    final DesignerComponent dc = comp.getAnnotation(DesignerComponent.class);
+
+    // The reason behind using a string builder here instead of the YAML library that's being used
+    // for the processor is that that library provides only immutable builders to construct YAML,
+    // and you cannot add fields to it after it has been instantiated.
+    // This is sort of a work-around. It'd be much better to just use some other YAML parsing library
+    // for the whole project.
+    final StringBuilder content = new StringBuilder();
+    content.append(moreInfo + "\n---\n")
+        .append("name: " + extName + "\n");
+
+    content.append("description: ")
+        .append(!dc.description().equals("") ? dc.description()
+            : "Extension component for " + extName + ". Built with Rush.")
+        .append("\n\n");
+
+    content.append("version: \n")
+        .append("  number: " + dc.version() + "\n")
+        .append("  name: ")
+        .append(!dc.versionName().equals("") ? dc.versionName() : dc.version())
+        .append("\n\n");
+
+    content.append(optimizeComment + "\n");
+    content.append("min_sdk: " + dc.androidMinSdk() + "\n\n");
+
+    content.append("assets: \n")
+        .append("  icon: ")
+        .append(!dc.iconName().equals("") ? dc.iconName() : "icon.png")
+        .append("\n");
+    final UsesAssets ua = comp.getAnnotation(UsesAssets.class);
+    if (ua != null) {
+      content.append("  other:\n");
+      for (final String file : ua.fileNames().split(",")) {
+        content.append("    - " + file.trim() + "\n");
+      }
+    }
+    content.append("\n");
+
+    final UsesLibraries ul = comp.getAnnotation(UsesLibraries.class);
+    if (ul != null) {
+      content.append("deps:\n");
+      for (final String lib : ul.libraries().split(",")) {
+        content.append("  - " + lib.trim() + "\n");
+      }
+      content.append("\n");
+    }
+
+    final FileWriter writer = new FileWriter(Paths.get(rushYmlPath).toFile());
+    writer.write(content.toString());
+    writer.flush();
+    writer.close();
+  }
+
+  /**
    * Generates AndroidManifest.xml for {@param comp}
    *
    * @param comp         the element for which AndroidManifest.xml is to be generated
-   * @param messager     the messager for this processing env
    * @param manifestPath the path to where the generated manifest file is to br stored.
    */
-  private void generateAndroidManifest(Element comp, Messager messager, String manifestPath) {
+  private void generateAndroidManifest(Element comp, String manifestPath) throws TransformerException, ParserConfigurationException {
     final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    DocumentBuilder documentBuilder = null;
-    try {
-      documentBuilder = dbf.newDocumentBuilder();
-    } catch (ParserConfigurationException e) {
-      e.printStackTrace();
-    }
-
-    if (documentBuilder == null) {
-      messager.printMessage(Diagnostic.Kind.ERROR, "ERR Something went wrong while instantiating document builder.");
-    }
+    DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
 
     final Document doc = documentBuilder.newDocument();
     doc.setXmlVersion("1.0");
@@ -185,11 +255,9 @@ public class Migrator extends AbstractProcessor {
     final StreamResult streamResult = new StreamResult(Paths.get(manifestPath).toFile());
 
     final TransformerFactory tf = TransformerFactory.newInstance();
-    try {
-      final Transformer transformer = tf.newTransformer();
-      transformer.transform(domSource, streamResult);
-    } catch (TransformerException e) {
-      e.printStackTrace();
-    }
+    final Transformer transformer = tf.newTransformer();
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+    transformer.transform(domSource, streamResult);
   }
 }
