0eb217ef49c3112785db4dce8871f50b037fd37f
Shreyash <saitwalshreyash19@gmail.com>
2021-08-09 16:09:07 +0530

    chore(annotations): migrate to Kotlin

---
+++ annotations/src/main/java/com/google/appinventor/components/annotations/SimpleEvent.kt
@@ -0,0 +1,19 @@
+package com.google.appinventor.components.annotations
+
+/**
+ * Annotation to mark Simple event definitions.
+ *
+ */
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+annotation class SimpleEvent(
+    /**
+     * If non-empty, description to use in user-level documentation in place of
+     * Javadoc, which is meant for developers.
+     */
+    val description: String = ""
+)
